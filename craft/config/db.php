c924278e1b6afecb1062625cbbe6f283dbf77170
xjnny <jennylcrowther@gmail.com>
2018-02-11 02:34:42 +1300

    dasd

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,21 +7,35 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
-return array(
+// return array(
 
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
+// 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+// 	'server' => 'localhost',
 
-	// The name of the database to select.
-	'database' => 'portfolio_2018',
+// 	// The name of the database to select.
+// 	'database' => 'portfolio_2018',
 
-	// The database username to connect with.
-	'user' => 'root',
+// 	// The database username to connect with.
+// 	'user' => 'root',
 
-	// The database password to connect with.
-	'password' => 'root',
+// 	// The database password to connect with.
+// 	'password' => 'root',
 
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
+// 	// The prefix to use when naming tables. This can be no more than 5 characters.
+// 	'tablePrefix' => 'craft',
 
-);
+// );
+
+
+
+return [
+    'server'      => getenv('MYSQL_HOST')     ?: 'localhost',
+    'database'    => getenv('MYSQL_DATABASE') ?: 'local-db-name',
+    'user'        => getenv('MYSQL_USER')     ?: 'local-db-user',
+    'password'    => getenv('MYSQL_PASSWORD') ?: 'local-db-password',
+    'tablePrefix' => 'craft',
+
+    // to be compatible with MySQL 5.7, requires 2.6.2949 or greater
+    'initSQLs'     => ["SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"],
+
+];
